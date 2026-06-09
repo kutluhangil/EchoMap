ae511b69e3c8adcd7e9771ac5d731fb2a165c958
kutluhangil <kutluhangul@windowslive.com>
2026-06-09 23:06:01 +0300

    chore: set up ESLint, Prettier, and Husky pre-commit hooks
    Pre-commit runs lint-staged (eslint --fix + prettier) and a full typecheck
    to keep main green.

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+  expoConfig,
+  // Disable stylistic rules that Prettier owns to avoid conflicts.
+  eslintConfigPrettier,
+  {
+    ignores: ['dist/*', '.expo/*', 'node_modules/*'],
+  },
+]);
